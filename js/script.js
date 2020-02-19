1b7a00a3c1fda76aeef239a742a6e43c1cbf0679
alondrajjaur <alondra.j.2003@gmail.com>
2020-02-19 02:22:36 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ var list = ["7 rings" , "thank u next" , "problem"]
 list.forEach(function(song) {
  $(".myList").append('<p>' + song +'</p>') 
 })
+
+var artist = ["ariana" , "ariana" , "ariana"]
+artist.forEach(function(song){
+    $(".myArtist").append('<p>' + song +'</p>')
+})
